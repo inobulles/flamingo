78c4357cf031746bbd8dababb0615bf4bf238946
Aymeric Wibo <obiwac@gmail.com>
2024-07-13 19:46:20 +0200

    flamingo: Swap `parse_literal` and `parse_identifier`

--- flamingo/flamingo.c
+++ flamingo/flamingo.c
@@ -160,28 +160,6 @@ static int parse_list(flamingo_t* flamingo, TSNode node) {
 	return 0;
 }
 
-static int parse_identifier(flamingo_t* flamingo, TSNode node, flamingo_val_t** val) {
-	assert(strcmp(ts_node_type(node), "identifier") == 0);
-	assert(ts_node_child_count(node) == 0);
-
-	size_t const start = ts_node_start_byte(node);
-	size_t const end = ts_node_end_byte(node);
-
-	char const* const identifier = flamingo->src + start;
-	size_t const size = end - start;
-
-	flamingo_var_t* const var = flamingo_scope_find_var(flamingo, identifier, size);
-
-	if (var == NULL) {
-		return error(flamingo, "could not find identifier: %.*s", (int) size, identifier);
-	}
-
-	*val = var->val;
-	val_incref(*val);
-
-	return 0;
-}
-
 static int parse_literal(flamingo_t* flamingo, TSNode node, flamingo_val_t** val) {
 	assert(strcmp(ts_node_type(node), "literal") == 0);
 	assert(ts_node_child_count(node) == 1);
@@ -229,6 +207,28 @@ static int parse_literal(flamingo_t* flamingo, TSNode node, flamingo_val_t** val
 	}
 
 	return error(flamingo, "unknown literal type: %s", type);
+}
+
+static int parse_identifier(flamingo_t* flamingo, TSNode node, flamingo_val_t** val) {
+	assert(strcmp(ts_node_type(node), "identifier") == 0);
+	assert(ts_node_child_count(node) == 0);
+
+	size_t const start = ts_node_start_byte(node);
+	size_t const end = ts_node_end_byte(node);
+
+	char const* const identifier = flamingo->src + start;
+	size_t const size = end - start;
+
+	flamingo_var_t* const var = flamingo_scope_find_var(flamingo, identifier, size);
+
+	if (var == NULL) {
+		return error(flamingo, "could not find identifier: %.*s", (int) size, identifier);
+	}
+
+	*val = var->val;
+	val_incref(*val);
+
+	return 0;
 }
 
 static int parse_expr(flamingo_t* flamingo, TSNode node, flamingo_val_t** val) {
