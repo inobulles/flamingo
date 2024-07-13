051bf4b1a21c0502388e09c5e4d7d48a812bb078
Aymeric Wibo <obiwac@gmail.com>
2024-07-13 21:59:45 +0200

    questions: Answer one of my questions (and come up with a new one :P)

--- flamingo/flamingo.c
+++ flamingo/flamingo.c
@@ -259,7 +259,26 @@ static int parse_call(flamingo_t* flamingo, TSNode node, flamingo_val_t** val) {
 	}
 
 	// Actually call the callable.
-	// TODO Should I really be passing TSNode everywhere instead of TSNode*? It feels like this is prone to me being like "oh, well, the TSNode type is probably hiding a pointer if it's being used as an argument, so I don't have to worry about out-of-scope usage".
+
+	/* TODO
+		The issue with this is that the body we're calling may further down the scope stack, and blocks just blindly push a new scope on top of the stack.
+	   What we should really be doing is copying the scope stack, rolling it back to the scope where the function was declared, and then pushing a new scope on top of that.
+	   Maybe there's a better way? Here's an illustration of the problem:
+
+		fn fun2() {
+			print(a) # I should not have access to 'a' in here.
+		}
+
+		fn fun1() {
+			fn fun3() {
+				print(a) # In fact, and this is a semi-unrelated issue, I should have access to 'a' here either.
+			}
+
+			a = "hello"
+			fun2()
+			fun3()
+		}
+	*/
 
 	TSNode* const body = callable->fn.body;
 	return parse_statement(flamingo, *body);
---
+++ questions.md
@@ -0,0 +1,8 @@
+# Questions
+
+These are questions I've had during development, and that I've found answers to.
+I'm keeping track of these so I don't forget why I did something a certain way and am compelled to erroneously change it.
+
+*Q:* Should I really be passing TSNode everywhere instead of TSNode*? It feels like this is prone to me being like "oh, well, the TSNode type is probably hiding a pointer if it's being used as an argument, so I don't have to worry about out-of-scope usage".
+
+*A:* Yes. Tree-sitter passes them as values, so be consistent with that.
